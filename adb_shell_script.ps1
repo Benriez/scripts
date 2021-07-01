8978e4545cef43acda74ab98d551dd1117344150
Benriez <33022072+Benriez@users.noreply.github.com>
2021-07-01 14:56:31 +0200

    initial commit

---
+++ adb_shell_script.ps1
@@ -0,0 +1,16 @@
+Write-Host "Start Script"
+Write-Host "---------------"
+Write-Host "load adb"
+
+
+
+
+adb push sample/. /sdcard
+adb shell ls /sdcard
+adb shell
+
+
+Write-Host "---------------"
+Write-Host "Killing Server"
+adb kill-server
+
---
+++ adb_shell_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "START SCRIPT"
+echo "---------------------------------"
+
+
+#path to sdk platform tools
+# cd "/mnt/c/Users/Benny/AppData/Local/Android/Sdk/platform-tools"
+
+# echo $PWD
+# exec bash
+
+
+adb shell
+
+
+
