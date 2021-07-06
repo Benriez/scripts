39b7f3f1f19c2aed2421eaafc73f771989e32921
Benriez <33022072+Benriez@users.noreply.github.com>
2021-07-06 19:04:12 +0200

    update

--- adb_shell_script.ps1
+++ adb_shell_script.ps1
@@ -4,17 +4,29 @@ Write-Host "load adb"
 
 
 
-
-adb push sample/. /sdcard
-adb shell ls /sdcard
-adb shell
-
+Write-Host "...pushing F360 folder to sdcard"
+#adb push ./sdcard/F360Data /storage/9C33-6BBD
+Write-Host "filetransfer complete"
 
 
+Write-Host "installing app"
+#adb install ./2011041200_Alpha_skPATCH_signatured_1_1604490301.apk
+#path /sdcard/Android/data/de.fahrschule360.fahrschule360Alpha
+Write-Host "app installed"
+
+adb shell pm disable-user com.pvr.launcher
+adb shell pm grant de.fahrschule360.fahrschule360Alpha android.permission.WRITE_SECURE_SETTINGS
+#adb shell
 
 
 
-Write-Host "---------------"
-Write-Host "Killing Server"
+
+
+
+# Kill server
+Write-Host "---------------------------------"
+Write-Host "Killing ADB Server"
+Write-Host "---------------------------------"
+
 adb kill-server
 
--- adb_shell_script.sh
+++ adb_shell_script.sh
@@ -1,17 +1,45 @@
 #!/bin/bash
 
+echo "---------------------------------"
 echo "START SCRIPT"
 echo "---------------------------------"
 
 
-#path to sdk platform tools
-cd "/mnt/c/Users/Benny/AppData/Local/Android/Sdk/platform-tools"
-
-# echo $PWD
+# path to sdk platform tools
+# cd "/mnt/c/Users/Benny/AppData/Local/Android/Sdk/platform-tools"
 # exec bash
 
 
-adb shell
+
+# Check device
+adb devices
 
 
 
+# ADB Commands
+
+# create necessary subfolders 
+# adb push cannot create subfolders
+# adb shell mkdir /storage/9C33-6BBD/folder
+
+
+echo "...pushing F360 folder to sdcard"
+#adb push ./sdcard/F360Data /storage/9C33-6BBD
+echo "filetransfer complete"
+
+
+echo "installing app"
+adb install ./2011041200_Alpha_skPATCH_signatured_1_1604490301.apk
+echo "app installed"
+
+
+adb shell 
+
+
+
+# Kill server
+echo "---------------------------------"
+echo "Killing ADB Server"
+echo "---------------------------------"
+
+adb kill-server
